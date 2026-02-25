e7f9277c96a7c391d2c0ac893abd57a4d20ec971
omarbougarne <omarbougarne98@gmail.com>
2026-02-25 14:27:55 +0000

    feat(auth): complete JWT module configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { JwtStrategy } from './strategies/jwt.strategy';
+import type { StringValue } from 'ms';
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService): JwtModuleOptions => {
+                const secret = configService.get<string>('JWT_SECRET');
+                if (!secret) throw new Error('JWT_SECRET is missing in environment variables');
+                const expiresIn = (configService.get<string>('JWT_EXPIRES_IN') ?? '1d') as StringValue;
+
+                return {
+                    secret,
+                    signOptions: {
+                        expiresIn,
+                    },
+                };
+            },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+    exports: [JwtModule],
 })
-export class AuthModule {}
+export class AuthModule { }
